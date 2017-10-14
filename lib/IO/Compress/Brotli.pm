f8a89233332ac1ddd842918a9120d6ac19701aa3
Marius Gavrilescu <marius@ieval.ro>
2017-10-14 18:55:08 +0100

    Bump version and update Changes

--- lib/IO/Compress/Brotli.pm
+++ lib/IO/Compress/Brotli.pm
@@ -10,7 +10,7 @@ use IO::Uncompress::Brotli;
 our @EXPORT = qw/bro/;
 our @EXPORT_OK = @EXPORT;
 
-our $VERSION = '0.003_001';
+our $VERSION = '0.003_002';
 
 my %BROTLI_ENCODER_MODE = ( generic => 0, text => 1, font => 2 );
 sub mode {
--- lib/IO/Uncompress/Brotli.pm
+++ lib/IO/Uncompress/Brotli.pm
@@ -8,7 +8,7 @@ use parent qw/Exporter/;
 our @EXPORT = qw/unbro/;
 our @EXPORT_OK = @EXPORT;
 
-our $VERSION = '0.003001';
+our $VERSION = '0.003_002';
 
 require XSLoader;
 XSLoader::load('IO::Compress::Brotli', $VERSION);
