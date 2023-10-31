b0c240ea26d52c50bf5755e417547c0e0c09da01
Timothy Legge <timlegge@gmail.com>
2023-10-31 16:51:37 -0300

    Increment version and Update Changes for 0.017

--- lib/IO/Uncompress/Brotli.pm
+++ lib/IO/Uncompress/Brotli.pm
@@ -8,7 +8,7 @@ use parent qw/Exporter/;
 our @EXPORT = qw/unbro/;
 our @EXPORT_OK = @EXPORT;
 
-our $VERSION = '0.016';
+our $VERSION = '0.017';
 
 require XSLoader;
 XSLoader::load('IO::Compress::Brotli', $VERSION);
